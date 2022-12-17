1c23a039aaf9c093c0db367e10ff966488589264
suraj Yadav <surajyadav.sy@outlook.com>
2022-12-17 18:50:41 +0530

    dau-5 work

--- src/login.js
+++ src/login.js
@@ -1,9 +1,27 @@
-let userdata = document.getElementById("form");
-console.log(userdata);
-userdata.addEventListener("click", (e) => {
-  //   e.preventDefault();
+let userLogDetails = document.getElementById("form");
+let userData = JSON.parse(localStorage.getItem("userData")) || [];
+
+userLogDetails.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let flag = false;
+  for (let i = 0; i <= userData.length - 1; i++) {
+    if (
+      userData[i].Email === form.email.value &&
+      userData[i].Password === form.pass.value
+    ) {
+      flag = true;
+      break;
+    }
+  }
+  if (flag === true) {
+    alert("Login Successfull");
+    window.location.href = "./index.html";
+  } else {
+    alert("Wrong Credentials");
+  }
 });
 
+// PassWord visibility code
 let passbox = document.getElementById("pass");
 let showpass = document.getElementById("showpassWord");
 showpass.addEventListener("click", () => {
